2cb1f7b27c806932cf735856933428f7eda6fe3c
asadmahmood <asad.mahmood@norgic.com>
2023-02-03 18:12:43 +0500

    changed project id

--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -48,7 +48,7 @@ export class SignUpComponent implements OnInit {
     FormsHandler.validateForm(this.loginForm);
     if (this.loginForm.invalid) return;
     const saveData = this.loginForm.value;
-    saveData.project_id = '6RRM3H';
+    saveData.project_id = '115G1WZI';
     this.loading = true;
     this.formError = null;
     this.auth.signup(saveData).subscribe(v => {
